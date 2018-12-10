d5966c36ba895f6617a9424db4f842a86d3d10cd
Skylar Salerno <skylar.salerno@gmail.com>
2018-12-09 22:23:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function printBadges(names){
+  for(let i=0;i<names.length; i++){
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+  }
+  return names;
+}
+
+function tailsNeverFails(){
+let i=1;
+let times=0;
+while (i>=0.5){ i=Math.random();times++;}
+return `You got ${times-1} tails in a row!`
+}
